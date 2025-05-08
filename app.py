32bd96e7f730cc02ed9babfd5493f9a4b0907c8f
Andrew Beshay <102611183+andrewbeshay25@users.noreply.github.com>
2025-05-08 15:08:58 -0400

    Pre-deployment push

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import Ridge
 
 @st.cache_data
@@ -187,6 +186,7 @@ def main():
                     st.success(f"Predicted target value: **{prediction:.2f}**")
             except ValueError:
                 st.error("Please enter only numbers, separated by commas.")
+            st.write("You entered:", input_df)
 
 if __name__ == '__main__':
     main()
